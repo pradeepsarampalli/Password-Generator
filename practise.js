8fe6789252cb4478d7bd6282539f0c2bb5ecb50a
pradeepsarampalli <pradeepsarampalli@gmail.com>
2025-01-22 19:29:04 +0530

    finalcommit

--- practise.js
+++
@@ -1,7 +0,0 @@
-const slider = document.getElementById("slider")
-slider.addEventListener("mousemove",event=>{
-    console.log(slider.value)
-})
-function result(){
-    console.log(slider.value)
-}
