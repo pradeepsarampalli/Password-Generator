3cb0a55a48f7025aaa5c4a9dcec198e2614416a5
pradeepsarampalli <pradeepsarampalli@gmail.com>
2025-01-27 23:13:50 +0530

    updatedui

--- passwordgenerator.js
+++ passwordgenerator.js
@@ -1,19 +1,15 @@
+const passwordType = document.querySelectorAll(".password-type-buttons input")
 const resultPassword = document.getElementById("resultPassword")
 const passwordLength = document.getElementById("passwordLen")
-const passwordLengthDisplay =document.getElementById("passwordLenDisplay");
-const numbers = document.getElementById("numbers")
-const symbols = document.getElementById("symbols")
-const Random = document.getElementById("Random")
-const Pin = document.getElementById("Pin")
-const Alphabets = document.getElementById("Alphabets")
+const passwordLengthDisplay = document.getElementById("passwordLenDisplay")
 
+let typeArray = ['0123456789','`!@#$%^&*()_+=-','ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']
+let numbers = '0123456789'
+let capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let smalls = 'abcdefghijklmnopqrstuvwxyz'
+let symbols = '`!@#$%^&*()_+=-'
 
-let nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let Capitals = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let smalls = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let specials = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", "\"", ",", ".", "<", ">", "/", "?", "\\", "|", "`", "~"]
-
-let selctionIndex;
+console.log(passwordType)
 let passwordLen = Number(passwordLength.value)
 passwordLengthDisplay.textContent = passwordLen
 
@@ -25,69 +21,33 @@ passwordLength.addEventListener("click",event=>{
     passwordLengthDisplay.textContent = passwordLength.value
     passwordLen=passwordLength.value
 })
+function generatePassword() {
+    let choice = "";
+    if (passwordType[0].checked)
+        choice = capitals + smalls + numbers + symbols;
+    else {
+        for (let i = 1; i < 4; i++) {
+            if (passwordType[i].checked) {
+                choice += typeArray[i - 1];
+            }
+        }
+    }
+    console.log(choice);
+    let password = '';
+    for (let i = 0; i < passwordLen; i++) {
+        password += choice[Math.floor(Math.random() * choice.length)];
+    }
+    console.log(password);
+    resultPassword.value = password;
+}
 
-let choice = "Random";
-Random.addEventListener("click",event=>{
-   choice = "Random";
-   Random.style="border-bottom:1 px solid gray"
-   Pin.style="border:none"
-   Alphabets.style="border:none"
-})
-Pin.addEventListener("click",event=>{
-    choice = "Pin";
-    Pin.style="border-bottom:1 px solid gray"
-    Random.style="border:none"
-    Alphabets.style="border:none"
-})
-Alphabets.addEventListener("click",event=>{
-    choice = "Alphabets"
-    Alphabets.style="border-bottom:1 px solid gray"
-    Pin.style="border:none"
-    Random.style="border:none"
-})
+function copyPassword(){
+    navigator.clipboard.writeText(resultPassword.textContent).then(()=>{
+        alert("Password copied")
+    }).catch(error=>{
+        alert("failed to copy",error)
+    })
+}
 
-function generatePassword(){
-    if(choice=="Random"){
-    let password = Capitals[Math.floor(Math.random()*Capitals.length)];
-    for(let i = 1;i<passwordLen;i++){
-        selctionIndex = Math.floor(Math.random()*4)
-        if(selctionIndex==0  && !numbers.checked)
-            password+=nums[Math.floor(Math.random()*nums.length)]
-        else if(selctionIndex==1)
-            password+=Capitals[Math.floor(Math.random()*Capitals.length)]
-        else if(selctionIndex==2 && !symbols.checked)
-            password+=specials[Math.floor(Math.random()*specials.length)]
-        else
-            password+=smalls[Math.floor(Math.random()*smalls.length)]  
-        if ((i + 1) % 20 === 0) {
-            password += '\n';
-        }
-    }
-    resultPassword.value=password;
-}
-else if(choice=="Pin"){
-    let password="";
-    for(let i = 1;i<passwordLen;i++){
-        if(!numbers.checked)
-            password+=nums[Math.floor(Math.random()*nums.length)] 
-        if ((i + 1) % 20 === 0) {
-            password += '\n';
-        }
-    }
-    resultPassword.value=password;
-}
-else{
-    let password = Capitals[Math.floor(Math.random()*Capitals.length)];
-    for(let i = 1;i<passwordLen;i++){
-        selctionIndex = Math.floor(Math.random()*2)
-        if(selctionIndex==0)
-            password+=Capitals[Math.floor(Math.random()*Capitals.length)]
-        else
-            password+=smalls[Math.floor(Math.random()*smalls.length)]  
-        if ((i + 1) % 20 === 0) {
-            password += '\n';
-        }
-    }
-    resultPassword.value=password;
-}
-}
+
+
